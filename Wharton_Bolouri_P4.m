6e7a0d37cd2ad9794834a74bbd4f84e17ce90555
WhartonP <70900133+WhartonP@users.noreply.github.com>
2020-11-03 15:04:47 -0600

    [WIP] Current as of 11/3

--- Wharton_Bolouri_P4.m
+++ Wharton_Bolouri_P4.m
@@ -3,34 +3,38 @@
 clear; close all;
 
 % fileName = input("Enter the name of the image with extention ex. Image.tif -> ", 's');
-imOrig = double(imread("Proj4.tif"));
+imOrig = imread("Proj4.tif");
 
 [nx, ny] = size(imOrig);
+
 FT = log(1 + abs(fftshift(fft2(double(imOrig)))));
 
 FT2 = fft2(double(imOrig));
 
-filter = 1 - (fspecial('gaussian', size(imOrig), 100));
-
 %for a butterworth filter
 % Initialize filter.
-filter1 = ones(2*nx-1,2*ny-1);
-filter2 = ones(2*nx-1,2*ny-1);
-filter3 = ones(2*nx-1,2*ny-1);
+% filter1 = ones(2*nx-1,2*ny-1);
+% filter2 = ones(2*nx-1,2*ny-1);
+% filter3 = ones(2*nx-1,2*ny-1);
+d1 = 6;
+d0 = 4;
+n = 30;
 
-for i = 1:2*nx-1
-    for j =1:2*ny-1
-        dist = ((i-(nx+1))^2 + (j-(ny+1))^2)^.5;
+for i = 1:nx
+    for j = 1:ny
+        dist = ((i-(nx/2))^2 + (j-(ny/2))^2)^.5;
         % Create Butterworth filter.
         filter1(i,j)= 1/(1 + (dist/d1)^(2*n));
-        filter2(i,j) = 1/(1 + (dist/d0)^(2*n));
-        filter3(i,j)= 1.0 - filter2(i,j);
-        filter3(i,j) = filter1(i,j).*filter3(i,j);
+%         filter2(i,j) = 1/(1 + (dist/d0)^(2*n));
+        filter3(i,j)= 1.0 - filter1(i,j);
+%         filter3(i,j) = filter1(i,j).*filter3(i,j);
     end
 end
 
-imSharpen = FT .* filter;
-
-finalImage = log(1 + abs(ifftshift(ifft2(double(imSharpen)))));
-
-imshow(finalImage);
+imSharpen = FT2 .* filter3;
+g = real(ifft(imSharpen));
+imshow(g);
+% 
+% finalImage = log(1 + abs(ifftshift(ifft2(double(imSharpen)))));
+% 
+% imshow(finalImage);
