ffdfcb4c4d822846982ac37979b69cbc71af1c4e
WhartonP <70900133+WhartonP@users.noreply.github.com>
2020-11-08 15:52:34 -0600

    Current code for submitting

--- Farshad_Part2.m
+++ Farshad_Part2.m
@@ -3,16 +3,32 @@ close all
 
 figure
 I = imread('Proj4.tif');
-imshow(I)
+% imshow(I)
 
-se = strel('disk',15);
-background = imopen(I,se);
+figure; imshow(imread("Proj4_uniform.tif"));
+pause()
+lightValue = mean(mean(I));
+% for i = 1:5:120
+% se = strel('disk',15);
+% se = strel('sphere',i);
+% background = imopen(I,se);
+% I2 = I - background;
+% I3 = (I2 + uint8(lightValue));  %Lightenning up the image
+
+% figure; imshow(I3);
+
+se2 = offsetstrel('ball',120,16,8);
+background2 = imopen(I, se2);
+I4 = I - background2;
+I5 = I4 + uint8(lightValue);
+figure; imshow(I5);
 %imshow(background)
 
-I2 = I - background;
+
 %imshow(I2)
 
-figure
-I3 = (I2 + uint8(100));  %Lightenning up the image
-imshow(I3)
+% figure
+% I3 = (I2 + uint8(lightValue));  %Lightenning up the image
+% imshow(I3)
+% end
 
--- Wharton_Bolouri_P4.m
+++ Wharton_Bolouri_P4.m
@@ -2,41 +2,78 @@
 %the image and then correct the non-uniform illumination 
 clear; close all;
 
-% fileName = input("Enter the name of the image with extention ex. Image.tif -> ", 's');
-imOrig = imread("Proj4.tif");
-figure; imshow(imOrig);
+fileName = input("Enter the name of the image with extention ex. Image.tif -> ", 's');
+imOrig = imread(fileName);
+% figure; imshow(imOrig);
 
-FT = fftshift(fft2(double(imOrig)));
-FTView = (1 + abs(FT));
-figure; plot(FTView);
 
-%for a butterworth filter
-% Initialize filter.
-% filter1 = ones(2*nx-1,2*ny-1);
-% filter2 = ones(2*nx-1,2*ny-1);
-% filter3 = ones(2*nx-1,2*ny-1);
-[nx, ny] = size(imOrig);
-d1 = 400;
-d0 = 2^8;
-n = 2;
+%For extracting the pattern
+FT = (fftshift(fft2((imOrig))));
+ave = mean(mean(FT));
+% imagesc(log(1+abs(FT)))
+% colormap("gray");
+% colorbar()
 
-for i = 1:nx
-    for j = 1:ny
-        dist = ((i-(nx/2))^2 + (j-(ny/2))^2)^.5;
-        % Create Butterworth filter.
-        filter1(i,j)= 1/(1 + (dist/d1)^(2*n));
-        filter2(i,j) = 1/(1 + (dist/d0)^(2*n));
-        filter3(i,j)= 1.0 - filter2(i,j);
-        filter3(i,j) = filter1(i,j).*filter3(i,j);
+
+% Finding Spikes
+temp = FT;
+spikes = 7;
+indices = zeros(spikes,2);
+i = 1;
+while i < 8
+    [row, col] = find(temp == max(max(temp)));
+    if i == 1
+        maxRow= row;
+        maxCol = col;
+    end
+    if isempty((find(row == indices(:,1))) == 0) &&...
+            isempty((find(col == indices(:,2))) == 0)
+        indices(i,:) = [row, col];
+        i = i + 1;
+    end
+    temp(row, col) = 0;
+end
+
+flag = true;
+for i = 1:size(FT,1)
+    for j = 1:size(FT,2)
+        for k = 1:spikes
+            if FT(i,j) == FT(indices(k,1), indices(k,2))
+                flag =false;
+                if FT(i,j) ~= FT(maxRow, maxCol)
+                    FT(i,j) = FT(i,j) * 10;
+                end
+                break;
+            end
+        end
+        if flag == true
+            FT(i,j) = ave;
+        end
+        flag = true;
     end
 end
 
-imSharpen = filter3 .* FT;
-imSharpenView = log(1 + abs(imSharpen));
-figure; plot(imSharpenView);
+% 
+% figure
+% imagesc(log(1+abs(FT)))
+% colorbar()
+% colormap("gray")
 
-g = real(ifft2(fftshift(imSharpen)));
-figure; imshow(g);
+finalPatternImg = fftshift(ifft2(FT));
+finalPatternImg = imadjust(uint8(abs(finalPatternImg)));
 
+%For correcting the non-uniform lighting
 
+lightValue = mean(mean(imOrig));
+se = offsetstrel('ball',120,16,8);
+background = imopen(imOrig, se);
+imMinusBack = imOrig - background;
+finalBrightImg = imMinusBack + uint8(lightValue);
+
+subplot(2, 2, 1); imshow(imOrig);
+title('Original Input Image');
+subplot(2, 2, 2); imshow(finalPatternImg);
+title('Extracted Pattern');
+subplot(2 ,2, 3); imshow(finalBrightImg);
+title('Corrected non-uniform Lighting');
 
